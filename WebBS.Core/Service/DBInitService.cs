b02737ab83f02b412a1f42b69cf9087f437fa59f
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2022-02-01 19:03:12 +0100

    Minor price calculation test on product.

--- WebBS.Core/Service/DBInitService.cs
+++ WebBS.Core/Service/DBInitService.cs
@@ -6,7 +6,7 @@ namespace WebBS.Core.Service
     {
         public static void Init(WebBSContext context)
         {
-            var item = new Item { Name = "Harddrive", Id = 1 };
+            var item = new Item { Name = "Harddrive", Name2 = "Western Digital 8TB", Id = 1, CostPrice = 123.25, Coverage = 25, Price = 112, Supplier = "Super supplier"};
 
             context.Items.Add(item);
 
